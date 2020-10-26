98f7f5f7c91fa0a51ac2fc2fe72e469ea1d0b16a
zhz364 <zhz364@ucsd.edu>
2020-10-26 14:36:06 -0700

    Update css

--- frontend/src/components/forms/login_form.jsx
+++ frontend/src/components/forms/login_form.jsx
@@ -11,7 +11,10 @@ class LoginForm extends React.Component {
       password: '',
       errors: {}
     };
-    
+    this.password = React.createRef()
+    this.username= React.createRef()
+    this.password2 = React.createRef()
+
     this.handleSubmit = this.handleSubmit.bind(this);
     this.renderErrors = this.renderErrors.bind(this);
   }
@@ -57,35 +60,46 @@ class LoginForm extends React.Component {
     return (
       <div className="form-container">
         <form onSubmit={this.handleSubmit}>
-  
-              <input id="input-1"
-                type="text"
-                value={this.state.username}
-                onChange={this.update('username')}
-                placeholder="Username"
-                autoFocus
-              />
-              <label for="input-1">
-                <span className="label-text">Username</span>
-                <span className="nav-dot"></span>
-                <div className="signup-button-trigger">Sign up</div>
-              </label>
+          <input id="input-1" type="text"
+            value={this.state.username}
+            onChange={this.update('username')}
+            placeholder="Username"
+            // autoFocus
+          />
+          <label for="input-1">
+            <span className="label-text">Username</span>
+            <span className="nav-dot"></span>
+            <div className="signup-button-trigger">Log in</div>
+          </label>
 
-              <input id="input-2"
-                type="password"
-                value={this.state.password}
-                onChange={this.update('password')}
-                placeholder="Password"
-              />
-              <label for="input-2">
-                <span className="label-text">Password</span>
-                <span className="nav-dot"></span>
-              </label>
-            
-              <button className="btn" type="submit">Sign in</button>
-              <p className="tip">Press Tab</p>
-            {this.renderErrors()}
+          <input id="input-2"
+            type="password"
+            value={this.state.password}
+            onChange={this.update('password')}
+            placeholder="Password"
+          />
+          <label for="input-2">
+            <span className="label-text">Password</span>
+            <span className="nav-dot"></span>
+          </label>
 
+          <input id="input-3"
+            type="password"
+            value={this.state.password2}
+            onChange={this.update('password2')}
+            // placeholder="Confirm Password"
+            placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
+          />
+
+          <label for="input-3">
+            <span className="label-text">Confirm Password</span>
+            <span className="nav-dot"></span>
+          </label>
+        
+          <button className="btn" type="submit">Sign in</button>
+          <p className="tip">Press Tab</p>
+          <div className="signup-button">Log in</div>
+          {this.renderErrors()}
         </form>
       </div>
     );
--- frontend/src/components/forms/signup_form.jsx
+++ frontend/src/components/forms/signup_form.jsx
@@ -12,7 +12,7 @@ class SignupForm extends React.Component {
       errors: {}
     };
     this.password = React.createRef()
-    this.email = React.createRef()
+    this.username= React.createRef()
     this.password2 = React.createRef()
     
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -64,53 +64,49 @@ class SignupForm extends React.Component {
     return (
       <div className="form-container">
         <form onSubmit={this.handleSubmit}>
-              <input id="input-1" type="text"
-                value={this.state.username}
-                onChange={this.update('username')}
-                placeholder="Username"
-                
-              />
+          <input id="input-1" type="text"
+            value={this.state.username}
+            onChange={this.update('username')}
+            placeholder="Username"
+          />
 
-              <label for="input-1">
-                <span className="label-text">Username</span>
-                {/* <span className="label-text">Hello</span> */}
-                <span className="nav-dot"></span>
-                <div className="signup-button-trigger">Sign up</div>
-              </label>
-            
-            
-           
-              <input id="input-2"
-                type="password"
-                value={this.state.password}
-                onChange={this.update('password')}
-                ref={this.password}
-                // placeholder="Password"
-                placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
-              />
-              <label for="input-2">
-                <span className="label-text">Password</span>
-                <span className="nav-dot"></span>
-              </label>
-            
-              <input id="input-3"
-                type="password"
-                value={this.state.password2}
-                onChange={this.update('password2')}
-                // placeholder="Confirm Password"
-                placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
-              />
+          <label for="input-1">
+            <span className="label-text">Username</span>
+            <span className="nav-dot"></span>
+            <div className="signup-button-trigger">Sign up</div>
+          </label>
+        
+          <input id="input-2"
+            type="password"
+            value={this.state.password}
+            onChange={this.update('password')}
+            ref={this.password}
+            // placeholder="Password"
+            placeholder="Password"
+          />
+          <label for="input-2">
+            <span className="label-text">Password</span>
+            <span className="nav-dot"></span>
+          </label>
+        
+          <input id="input-3"
+            type="password"
+            value={this.state.password2}
+            onChange={this.update('password2')}
+            // placeholder="Confirm Password"
+            placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;"
+          />
 
-              <label for="input-3">
-                <span className="label-text">Confirm Password</span>
-                <span className="nav-dot"></span>
-              </label>
+          <label for="input-3">
+            <span className="label-text">Confirm Password</span>
+            <span className="nav-dot"></span>
+          </label>
             
-            {/* <input type="submit" value="Submit" /> */}
-            <button className="btn" type="submit">Create your Account</button>
-            <p className="tip">Press Tab</p>
-            <div className="signup-button">Sign up</div>
-            {this.renderErrors()}
+          {/* <input type="submit" value="Submit" /> */}
+          <button className="btn" type="submit">Create your Account</button>
+          <p className="tip">Press Tab</p>
+          <div className="signup-button">Sign up</div>
+          {this.renderErrors()}
         </form>
       </div>
     );
