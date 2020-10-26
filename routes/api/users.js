699d2dcde9b7abb61f1d1f05728880d57f22a444
dxu5 <derek_xu@brown.edu>
2020-10-26 13:42:18 -0700

    Finish register user route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,39 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const User = require("../../models/User");
 
 router.get("/test", (req, res) => {
   res.json({ msg: "This is the user route" });
+});
+
+router.post("/register", (req, res) => {
+  // Check to make sure nobody has already registered with a duplicate username
+  User.findOne({ username: req.body.username }).then((user) => {
+    if (user) {
+      // Throw a 400 error if the username already exists
+      return res
+        .status(400)
+        .json({ email: "A user has already registered with this username" });
+    } else {
+      // Otherwise create a new user
+      const newUser = new User({
+        username: req.body.username,
+        password: req.body.password,
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then((user) => res.json(user))
+            .catch((err) => console.log(err));
+        });
+      });
+    }
+  });
 });
 
 module.exports = router;
