3bae9a176e396cf61798d05a3c03b54fd457f45d
dxu5 <derek_xu@brown.edu>
2020-10-26 14:20:16 -0700

    Add passport to project

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
 const users = require("./routes/api/users");
 const bodyParser = require("body-parser");
+const passport = require("passport");
 
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to mongoDB"))
   .catch((err) => console.log(err));
+
+app.use(passport.initialize());
+require("./config/passport")(passport);
 
 app.use(
   bodyParser.urlencoded({
