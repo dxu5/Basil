8d260589f12a654107561924350f28f8b3ffd92a
dxu5 <derek_xu@brown.edu>
2020-10-26 11:43:32 -0700

    Add run scripts for development

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello Word!");
 });
 
 const port = process.env.PORT || 5000;
