dc30c9ce36b731dc25ceb8816fa2a6617c5268ef
llam <llam@g.hmc.edu>
2020-10-26 23:58:46 -0700

    create home page container

---
+++ frontend/src/components/home/home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Home extends React.Component{
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>HomePage</h1>
+            </div>
+        )
+    }
+}
+
+export default Home
