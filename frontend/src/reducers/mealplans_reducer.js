c40c382f7194a3b9cafd572937c793b2ca5df6d2
dxu5 <derek_xu@brown.edu>
2020-10-27 14:29:17 -0700

    Add receive current user action to mealplans reducer

--- frontend/src/reducers/mealplans_reducer.js
+++ frontend/src/reducers/mealplans_reducer.js
@@ -1,9 +1,11 @@
 import { RECEIVE_MEAL_PLAN } from "../actions/mealplan_actions";
+import { RECEIVE_CURRENT_USER } from "../actions/session_actions";
 
 const _nullState = {
   currentMealplan: undefined,
   prevMealplans: [],
   prospectiveMealplan: undefined,
+  currentMealplanStartTime: undefined,
 };
 
 const mealplansReducer = (state = _nullState, action) => {
@@ -12,6 +14,10 @@ const mealplansReducer = (state = _nullState, action) => {
   switch (action.type) {
     case RECEIVE_MEAL_PLAN:
       newState.prospectiveMealplan = action.mealplan;
+      return newState;
+    case RECEIVE_CURRENT_USER:
+      newState.currentMealplan = action.currentMealplan;
+      newState.currentMealplanStartTime = action.currentMealplanStartTime;
       return newState;
     default:
       return state;
--- frontend/src/reducers/session_reducer.js
+++ frontend/src/reducers/session_reducer.js
@@ -1,10 +1,12 @@
-import { RECEIVE_CURRENT_USER, 
-         RECEIVE_USER_LOGOUT, 
-         RECEIVE_USER_SIGN_IN } from '../actions/session_actions';
+import {
+  RECEIVE_CURRENT_USER,
+  RECEIVE_USER_LOGOUT,
+  RECEIVE_USER_SIGN_IN,
+} from "../actions/session_actions";
 
 const initialState = {
   isAuthenticated: false,
-  user: {}
+  user: {},
 };
 
 const SessionReducer = (state = initialState, action) => {
@@ -13,16 +15,16 @@ const SessionReducer = (state = initialState, action) => {
       return {
         ...state,
         isAuthenticated: !!action.currentUser,
-        user: action.currentUser
+        user: action.currentUser,
       };
     case RECEIVE_USER_LOGOUT:
       return {
         isAuthenticated: false,
-        user: undefined
+        user: undefined,
       };
     default:
       return state;
   }
-}
+};
 
 export default SessionReducer;
