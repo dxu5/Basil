fbb57747f89ad9080cb39215d1a36851e6d3d9bb
dxu5 <derek_xu@brown.edu>
2020-10-26 17:29:04 -0700

    Add instructions

--- frontend/src/util/spoonacular_api_util.js
+++ frontend/src/util/spoonacular_api_util.js
@@ -2,6 +2,11 @@ import axios from "axios";
 
 export const apiBaseURL = "https://api.spoonacular.com";
 
+//details can include:
+//target calories (api will get as close to this value DAILY)
+//diet (there is a list on spoonacular)
+//exclude (comma separated list of lowercase ingredients)
+
 export const getWeeklyMealPlan = (details) => {
   return axios.get(`${apiBaseURL}/mealplanner/generate`, {
     params: {
@@ -11,6 +16,8 @@ export const getWeeklyMealPlan = (details) => {
     },
   });
 };
+
+//takes in a meal id and serves over 500 lines of information for the meal, including ingredients, instructions, equipment, pictures
 
 export const getMealInfo = (mealId) => {
   return axios.get(`${apiBaseURL}/recipes/${mealId}/information`, {
