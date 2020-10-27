ce79e6e83e0bced8caa5dd6c3bb54a0c6f6e793d
dxu5 <derek_xu@brown.edu>
2020-10-26 21:26:17 -0700

    Add action creators

--- frontend/src/actions/mealplan_actions.js
+++ frontend/src/actions/mealplan_actions.js
@@ -2,3 +2,17 @@ import * as MealplanAPIUtil from "../util/spoonacular_api_util";
 
 export const RECEIVE_MEAL_PLAN = "RECEIVE_MEAL_PLAN";
 export const RECEIVE_MEAL_INFO = "RECEIVE_MEAL_INFO";
+
+export const receiveMealPlan = (mealplan) => {
+  return {
+    type: RECEIVE_MEAL_PLAN,
+    mealplan,
+  };
+};
+
+export const receiveMealInfo = (meal) => {
+  return {
+    type: RECEIVE_MEAL_INFO,
+    meal,
+  };
+};
