a06e7fc695b1c7e6425a23ec720b25c96f50ed2d
zhz364 <zhz364@ucsd.edu>
2020-10-27 10:25:01 -0700

    Add Calories input

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
-import { Switch, Route, Redirect } from "react-router-dom";
-import NavBarContainer from "./navbar/navbar_container";
+import { Switch, Route } from "react-router-dom";
 
 import SplashPage from "./splash/splash_page";
 import LoginFormContainer from "./forms/login_form_container";
@@ -9,13 +8,11 @@ import SignupFormContainer from "./forms/signup_form_container";
 
 const App = () => (
   <div>
-    <NavBarContainer />
+    {/* <NavBarContainer /> */}
     <Switch>
-      {/* <ProtectedRoute path="/home" component /> */}
+      <Route exact path="/" component={SplashPage} />
       <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} />
-      <Route exact path="/" component={SplashPage} />
-      <Redirect to="/" />
     </Switch>
   </div>
 );
