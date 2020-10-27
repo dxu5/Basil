41ccd80f065d539e4d2e04690453f9cbd2cde732
nateoh93 <nathanoh93@gmail.com>
2020-10-26 17:48:06 -0700

    style navbar

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import { Switch, Route } from "react-router-dom";
+import NavBarContainer from './navbar/navbar_container'
 
 import SplashPage from "./splash/splash_page";
 import LoginFormContainer from './forms/login_form_container';
@@ -8,7 +9,7 @@ import SignupFormContainer from './forms/signup_form_container';
 
 const App = () => (
   <div>
-    {/* <NavBarContainer /> */}
+    <NavBarContainer />
     <Switch>
         <Route exact path="/" component={SplashPage} />
         <Route exact path="/login" component={LoginFormContainer} />
--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -27,8 +27,26 @@ class NavBar extends React.Component {
       } else {
         return (
             <div>
-                <Link to={'/signup'}>Signup</Link>
-                <Link to={'/login'}>Login</Link>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" 
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><Link to={'/signup'}>Signup</Link></a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><Link to={'/login'}>Login</Link></a>
+                        </li>
+                    </ul>
+                </div>
             </div>
         );
       }
@@ -36,9 +54,11 @@ class NavBar extends React.Component {
 
   render() {
       return (
-        <div>
-            <h1>Chirper</h1>
-            { this.getLinks() }
+        <div className='container'>
+            <nav className='navbar fixed-top navbar-dark bg-dark'>
+                <a class="navbar-brand" href="#">Logo</a>
+                { this.getLinks() }
+            </nav>
         </div>
       );
   }
