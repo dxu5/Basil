5f2a8932f95ddfc1eb3bb1479345e532e8a825ed
nateoh93 <nathanoh93@gmail.com>
2020-10-26 21:39:08 -0700

    navbar styling

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -18,7 +18,7 @@ class NavBar extends React.Component {
   getLinks() {
       if (this.props.loggedIn) {
         return (
-            <div>
+            <div className='navbar-links'>
                 <Link to={'/home'}>Home</Link>
                 <Link to={'/profile'}>Profile</Link>
                 <button onClick={this.logoutUser}>Logout</button>
@@ -26,27 +26,9 @@ class NavBar extends React.Component {
         );
       } else {
         return (
-            <div>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" 
-                    aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                
-                <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                    <ul class="navbar-nav">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Home<span class="sr-only">(current)</span></a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="#"><Link to={'/signup'}>Signup</Link></a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="#"><Link to={'/login'}>Login</Link></a>
-                        </li>
-                    </ul>
-                </div>
+            <div className='navbar-links'>
+                <a class="nav-link" href="#"><Link to={'/signup'}>Signup</Link></a>
+                <a class="nav-link" href="#"><Link to={'/login'}>Login</Link></a>
             </div>
         );
       }
@@ -55,7 +37,7 @@ class NavBar extends React.Component {
   render() {
       return (
         <div className='container'>
-            <nav className='navbar fixed-top navbar-dark bg-dark'>
+            <nav className='navbar fixed-top'>
                 <a class="navbar-brand" href="#"><img src="/logoDark.png"/></a>
                 { this.getLinks() }
             </nav>
