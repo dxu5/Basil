2f08647989b00976933d3cb4b57a5ca5eb112627
llam <llam@g.hmc.edu>
2020-10-27 22:18:39 -0700

    style user info icons

--- frontend/src/components/home/user_info.jsx
+++ frontend/src/components/home/user_info.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faMedal, faUtensils, faCheck, faFire, faHandHoldingHeart } from "@fortawesome/free-solid-svg-icons";
 // import "./user_info.css";
 
 class UserInfo extends React.Component {
@@ -14,26 +16,30 @@ class UserInfo extends React.Component {
     return (
       <div className="user-info-div">
         <div className="user-stat">
+          <FontAwesomeIcon icon={faUser} />
           <h5>I am</h5>
           <h5>{this.props.user.username}</h5>
         </div>
         <div className="user-stat">
+          <FontAwesomeIcon icon={faMedal} />
           <h5>I am level</h5>
-          <h5>{`${Math.floor(this.state.completed/20)} `}</h5>
+          <h5>{`${Math.floor(this.state.completed / 20)} `}</h5>
         </div>
         <div className="user-stat">
+          <FontAwesomeIcon icon={faUtensils} />
           <h5>I have made</h5>
           <h5>{`${this.state.completed} meals`}</h5>
         </div>
         <div className="user-stat">
+          <FontAwesomeIcon icon={faCheck} />
           <h5>I completed</h5>
           <h5>{`${this.state.completed} % of my meals`}</h5>
         </div>
         <div className="user-stat">
+          <FontAwesomeIcon icon={faHandHoldingHeart} />
           <h5>I averaged</h5>
           <h5>{`${this.state.completed} cal/meal`}</h5>
         </div>
-
       </div>
     );
   }
