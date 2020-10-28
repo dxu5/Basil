81130cbd7770065ee75cc5dedbebc518e1757027
dxu5 <derek_xu@brown.edu>
2020-10-27 18:30:31 -0700

    Remove debuggers

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -24,7 +24,6 @@ document.addEventListener("DOMContentLoaded", () => {
       id: decodedUser.id,
       username: decodedUser.username,
     };
-    debugger;
     const preloadedState = {
       entities: {
         mealplans: {
