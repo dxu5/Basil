ca62a8557f92503cae7358d14bbf17cc9f0985de
nateoh93 <nathanoh93@gmail.com>
2020-10-28 12:23:09 -0700

    add toggle to complete meal button

--- frontend/src/components/calendar/calendar_item.js
+++ frontend/src/components/calendar/calendar_item.js
@@ -6,24 +6,31 @@ import { faCheck } from "@fortawesome/free-solid-svg-icons";
 class CalendarItem extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            completed: false
+        }
         this.handleClick = this.handleClick.bind(this);
     }
 
     handleClick(e) {
-        
+        e.preventDefault();
+        let bool = this.state.completed
+        this.setState({completed: !bool})
     }
 
     render() {
         // debugger
+        let completeIcon = this.state.completed ? <div className='check-icon'><FontAwesomeIcon icon={faCheck} /></div> : null;
+        let uncompleteBtn = this.state.completed ? <button id='complete-btn' onClick={this.handleClick}>Undo</button> : <button id='complete-btn' onClick={this.handleClick}>Complete?</button>;
         return(
             <div className={`meal-${this.props.mealNum}`}>
                 <div className='meal-img'><img src={`https://spoonacular.com/recipeImages/${this.props.id}-636x393.${this.props.imageType}`}/></div>
                 <div className='meal-description'>
-                    <div className='check-icon'><FontAwesomeIcon icon={faCheck} /></div>
+                    {completeIcon}
                     <p className='meal-title'>{this.props.title}</p>
                     <p className='meal-time'>{this.props.readyInMinutes} minutes</p>
                 </div>
-                <button id='complete-btn' onClick={this.handleClick}>Completed?</button>
+                {uncompleteBtn}
             </div>
         )
     }
