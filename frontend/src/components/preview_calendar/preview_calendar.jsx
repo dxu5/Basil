b9b951be5c07e6eaebcf7eb5e18b0ca3cec3816b
dxu5 <derek_xu@brown.edu>
2020-10-29 11:57:52 -0700

    Add preview calendar item to state

--- frontend/src/components/preview_calendar/preview_calendar.jsx
+++ frontend/src/components/preview_calendar/preview_calendar.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import PreviewCalendarItem from "./preview_calendar_item.jsx";
 
 export default class PreviewCalendar extends Component {
   constructor(props) {
@@ -32,6 +33,53 @@ export default class PreviewCalendar extends Component {
     if (this.props.previewMealplan === undefined) {
       return null;
     }
-    return <div>Test Calendar</div>;
+    const displayMeal = (field) =>
+      this.state[field].meals.map((meal, idx) => {
+        return (
+          <PreviewCalendarItem
+            key={meal.id}
+            id={meal.id}
+            day={field}
+            imageType={meal.imageType}
+            title={meal.title}
+            readyInMinutes={meal.readyInMinutes}
+            mealNum={idx}
+          />
+        );
+      });
+    return (
+      <div className="calendar-container">
+        <table className="table">
+          <div className="day-container">
+            <p id="Sunday">Sunday</p>
+            {displayMeal("Sunday")}
+          </div>
+          <div className="day-container">
+            <p id="Monday">Monday</p>
+            {displayMeal("Monday")}
+          </div>
+          <div className="day-container">
+            <p id="Tuesday">Tuesday</p>
+            {displayMeal("Tuesday")}
+          </div>
+          <div className="day-container">
+            <p id="Wednesday">Wednesday</p>
+            {displayMeal("Wednesday")}
+          </div>
+          <div className="day-container">
+            <p id="Thursday">Thursday</p>
+            {displayMeal("Thursday")}
+          </div>
+          <div className="day-container">
+            <p id="Friday">Friday</p>
+            {displayMeal("Friday")}
+          </div>
+          <div className="day-container">
+            <p id="Saturday">Saturday</p>
+            {displayMeal("Saturday")}
+          </div>
+        </table>
+      </div>
+    );
   }
 }
