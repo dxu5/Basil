7043d75e96aeef54483953516d456756808c68f0
llam <llam@g.hmc.edu>
2020-10-29 20:59:28 -0700

    style show page

--- frontend/src/components/show/show.jsx
+++ frontend/src/components/show/show.jsx
@@ -17,54 +17,56 @@ class Show extends React.Component{
         if(!meal) {return null}
         return (
           <div className="show">
-              
-            <h1 className="show-header">{meal.title}</h1>
-            <section className="show-basics">
-              {/* <p>Title: </p> */}
-              <p>Ready in: {`${meal.readyInMinutes} minutes`}</p>
-              <p>Preparation Time: {`${meal.preparationMinutes}`}</p>
-              <p>Cooking Time: {`${meal.cookingMinutes} minutes`}</p>
-              <p>Servings: {meal.servings}</p>
-              {/* <p>Cuisine: {meal.cuisines.map(cuisine => {
-                        return <li>cuisine</li>
-                    })}</p> */}
-              <img src={meal.image} />
-            </section>
-            <section className="show-ingredients">
-              Ingredients
-              <p>
-                {meal.extendedIngredients.map((ingredient) => {
-                  return (
-                    <ul>
-                      <li>{ingredient.original}</li>
-                      <li>
-                        <img src={`https://spoonacular.com/cdn/ingredients_500x500/${ingredient.image}`}/>
-                      </li>
-                      <li>{ingredient.amount}</li>
-                      <li>{ingredient.unit}</li>
-                      <li>{ingredient.originalName}</li>
-                    </ul>
-                  );
-                })}
-              </p>
-            </section>
-            <section className="show-instructions">
-              Instructions
-              <p>Instructions: {meal.instructions}</p>
-              <p>
-                Steps:{" "}
-                {meal.analyzedInstructions[0].steps.map((stepObj) => {
-                  return <li>{stepObj.step}</li>;
-                })}
-              </p>
-            </section>
+            <div className="show-header">
+              <h1>{meal.title}</h1>
+            </div>
             <section className="show-summary">
-              Summary:
               <div
                 dangerouslySetInnerHTML={{ __html: `${meal.summary}` }}
               ></div>
-              
             </section>
+            <section className="show-basics">
+              <p>Total Time: {`${meal.readyInMinutes} minutes`}</p>
+              <p>Prep Time: {`${meal.preparationMinutes} minutes`}</p>
+              <p>Cook Time: {`${meal.cookingMinutes} minutes`}</p>
+              <p>Servings: {meal.servings}</p>
+            </section>
+            <img src={meal.image} className="show-img" />
+            <section className="show-ingredients">
+              <ul className="show-ing-list">
+                {meal.extendedIngredients.map((ingredient) => {
+                  return <li>{ingredient.original}</li>;
+                })}
+              </ul>
+              <div className="show-ing-imgs">
+                {meal.extendedIngredients.map((ingredient) => {
+                  return (
+                    <ul className="show-ing">
+                      <li>
+                        <img
+                          src={`https://spoonacular.com/cdn/ingredients_500x500/${ingredient.image}`}
+                        />
+                      </li>
+                      <div className="show-ing-img-info">
+                        <li>{Math.round(ingredient.amount * 100) / 100}</li>
+                        <li>{ingredient.unit}</li>
+                        <li>{ingredient.originalName}</li>
+                      </div>
+                    </ul>
+                  );
+                })}
+              </div>
+            </section>
+            <section className="show-instructions">
+              <p>{meal.instructions}</p>
+            </section>
+            <section className="show-steps">
+              Steps:{" "}
+              {meal.analyzedInstructions[0].steps.map((stepObj) => {
+                return <li>{stepObj.step}</li>;
+              })}
+            </section>
+
             {/* <section>Wine Pairing</section> */}
           </div>
         );
