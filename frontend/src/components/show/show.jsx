83e514ba526fc923c93beabc056d793ff7345d6b
llam <llam@g.hmc.edu>
2020-10-30 10:49:12 -0700

    style showpage

--- frontend/src/components/show/show.jsx
+++ frontend/src/components/show/show.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
-import './show.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUser,
+  faClock,
+  faUtensilSpoon,
+  faUtensils,
+  faFire,
+} from "@fortawesome/free-solid-svg-icons";
+import './show.css';
 
 class Show extends React.Component{
     constructor(props){
@@ -19,28 +27,47 @@ class Show extends React.Component{
               <h1>{meal.title}</h1>
             </div>
             <section className="show-summary">
-                <h2>About me</h2>
-                <div
-                    dangerouslySetInnerHTML={{ __html: `${meal.summary}` }}
-                ></div>
+              <h2>About me</h2>
+              <div
+                dangerouslySetInnerHTML={{ __html: `${meal.summary}` }}
+              ></div>
             </section>
             <section className="show-basics">
-              <p>Total Time: {`${meal.readyInMinutes} minutes`}</p>
-              <p>Prep Time: {`${meal.preparationMinutes} minutes`}</p>
-              <p>Cook Time: {`${meal.cookingMinutes} minutes`}</p>
-              <p>Servings: {meal.servings}</p>
+              <p>
+                <FontAwesomeIcon icon={faClock} />
+                {` ${meal.readyInMinutes} min.`}
+              </p>
+              {meal.preparationMinutes ? (
+                <p>
+                  <FontAwesomeIcon icon={faUtensils} />
+                  {` ${meal.preparationMinutes} min.`}
+                </p>
+              ) : null}
+              {meal.cookingMinutes ? (
+                <p>
+                  <FontAwesomeIcon icon={faFire} />
+                  {` ${meal.cookingMinutes} min.`}
+                </p>
+              ) : null}
+              <p>
+                <FontAwesomeIcon icon={faUser} />
+                {` ${meal.servings} servings`}
+              </p>
             </section>
             <section className="show-main">
-                <div className="show-left">
-                    <img src={meal.image} className="show-img" />
-                    <h3>Ingredients</h3>
-                    <ul className="show-ing-list">
-                        {meal.extendedIngredients.map((ingredient) => {
-                        return <li>{ingredient.original}</li>;
-                        })}
-                    </ul>
+              <div className="show-left">
+                <img src={meal.image} className="show-img" />
+                <div className="show-ing-container">
+                  <h2>Ingredients</h2>
+                  <ul className="show-ing-list">
+                    {meal.extendedIngredients.map((ingredient) => {
+                      return <li>{ingredient.original}</li>;
+                    })}
+                  </ul>
+                  <div className="show-left-bg"></div>
                 </div>
-            
+              </div>
+
               <div className="show-right">
                 {meal.extendedIngredients.map((ingredient) => {
                   return (
@@ -61,22 +88,36 @@ class Show extends React.Component{
               </div>
             </section>
             <section className="show-instructions">
-                <div className="read-me-img"></div>
-                <div>
-                    <h2>Read me</h2>
-                    <p>{meal.instructions}</p>
-                </div>
+              <div className="read-me-img">
+                <p className="quote">
+                  “We abide by the old adage — quality over quantity. While we
+                  now have a large database built over time, the quality of
+                  meals has always been at the forefront.”
+                  <span id="line-break"></span>-Zhang Zhang (Co-founder)
+                </p>
+              </div>
+              <div>
+                <h2>Read me</h2>
+                <p>{meal.instructions}</p>
+              </div>
             </section>
             <section className="show-steps">
-                <div>
-                    <h2>Follow me</h2>
-                    <ol>
-                        {meal.analyzedInstructions[0].steps.map((stepObj) => {
-                            return <li>{stepObj.step}</li>;
-                        })}
-                    </ol>
-                </div>
-                <div className="follow-me-img"></div>
+              <div>
+                <h2>Follow me</h2>
+                <ol>
+                  {meal.analyzedInstructions[0].steps.map((stepObj) => {
+                    return <li>{stepObj.step}</li>;
+                  })}
+                </ol>
+              </div>
+              <div className="follow-me-img">
+                <p className="quote">
+                  “My family and I are a walking testament that the process
+                  works, but rather than hear me talk about it we’ll let the
+                  results speak for themselves.”
+                  <span id="line-break"></span>-Derek Xu (Co-founder)
+                </p>
+              </div>
             </section>
 
             {/* <section>Wine Pairing</section> */}
